8664ad8505421a9a4df5688813130d96332092db
dapret <athallahdaffa936@gmail.com>
2023-10-03 09:56:33 +0700

    pengumpulan tugas jobsheet 3

---
+++ ContohVariabel21.java
@@ -0,0 +1,15 @@
+public class ContohVariabel21 {
+
+    public static void main(String[] args) {
+       String salahsatuHobySayaAdalah = "Bermain petak umpet";
+       boolean isPandai = true;
+       char jenisKelamin = 'L';
+       byte _umurSayaSekarang = 18;
+       double $ipk = 4.0, tinggi = 1.70;
+       System.out.println(salahsatuHobySayaAdalah);
+       System.out.println("Apakah pandai? " + isPandai);
+       System.out.println("Jenis kelamin: " + jenisKelamin);
+       System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+       System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+    }
+}
